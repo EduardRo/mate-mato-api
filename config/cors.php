856629ba4c22f06827d3091a2199fd124ba25dcf
EduardRo <eduardstancu@yahoo.com>
2025-02-12 00:03:07 +0200

    120225 modif

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => ['*',
     'http://localhost:5174/',
     'http://127.0.0.1:5173/',
     'http://localhost:5173',
